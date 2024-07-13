df81f75a44f0239f7c3fe81246f880b674626672
Reaper1177 <rasheed1.haddad@gmail.com>
2024-07-13 13:33:07 +0300

    added new page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Lecture_1 from './qudurat_math_pages/lecture_1.js';
 import Reading_1 from './qudurat_math_pages/reading_1.js';
 import Quiz_1 from './qudurat_math_pages/quiz_1.js';
 import Chapter_1 from './qudurat_math_pages/chapter_1.js';
+import Chapter_View from './qudurat_math_pages/chapter_view.js'; 
 
 function App() {
 
@@ -25,10 +26,11 @@ function App() {
 						
 						<Route path='qudurat_math' element={<Qudurat_Math />} />
 
-						<Route path='qudurat_math/Chapter_1' element={<Chapter_1 />} />
-							<Route path='qudurat_math/Chapter_1/Lecture_1' element={<Lecture_1 />} />
-							<Route path='qudurat_math/Chapter_1/Reading_1' element={<Reading_1 />} />
-							<Route path='qudurat_math/Chapter_1/Quiz_1' element={<Quiz_1 />} />
+						<Route path='qudurat_math/Qudurat_Math_Course' element={<Chapter_View />} />
+							<Route path='qudurat_math/Chapter_1' element={<Chapter_1 />} />
+								<Route path='qudurat_math/Chapter_1/Lecture_1' element={<Lecture_1 />} />
+								<Route path='qudurat_math/Chapter_1/Reading_1' element={<Reading_1 />} />
+								<Route path='qudurat_math/Chapter_1/Quiz_1' element={<Quiz_1 />} />
 
 						<Route path='*' element={<Err />}/>
 					</Routes>
